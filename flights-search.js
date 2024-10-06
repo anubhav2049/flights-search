d409777e805a53144d1d6b7e78dd4d704cf23c06
anubhav2049 <aadhik21@asu.edu>
2024-10-05 18:55:46 -0700

    Initial commit

---
+++ flights-search.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import { TextField, Button, MenuItem } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+
+const FlightSearch = () => {
+    const [origin, SetOrigin] = useState("");
+    const [destination, setDestination] = useState("");
+    const [departureDate, setDepartureDate] = useState(null);
+    const [returnDate, setReturnDate] = useState(null);
+    const [travelers, setTravelers] = useState(1);
+}
+
+return(
+    
+)
